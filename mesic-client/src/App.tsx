5afa6ebbd203590b4bbc7d6f370577d819cbeccf
Gwan Woo <gwanwoo@Gwans-MacBook-Pro.local>
2021-05-17 17:14:28 +0900

    redux useDispatch, useSelector 테스트

--- mesic-client/src/App.tsx
+++ mesic-client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import {useDispatch, useSelector} from "react-redux";
+import {onOff} from "./actions/index";
+import {RootState} from "./reducers";
 
 function App() {
+  const state = useSelector((state: RootState) => state.userReducer);
+  const dispatch = useDispatch();
+  const {show} = state;
+  const handleClick = (e: any) => {
+    dispatch(onOff(e.target.value));
+  };
   return (
     <div className="App">
-      Hello World!
+      <button id="button" onClick={handleClick} value={show}>
+        {show}
+      </button>
     </div>
   );
 }
