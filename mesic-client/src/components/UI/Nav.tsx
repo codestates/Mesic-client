109277d241ae18a68fc8c29acdfeb6380b02bf86
jinhw12 <jinhw12@naver.com>
2021-05-18 18:37:58 +0900

    Nav 버튼 만들기

--- mesic-client/src/components/UI/Nav.tsx
+++ mesic-client/src/components/UI/Nav.tsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../reducers";
 
-type ModalProps = {
-  open: boolean;
+type NavProps = {
+  //open: boolean;
 };
 
-function Nav(props: ModalProps) {
-  const {open} = props;
-  return <div>Nav</div>;
+function Nav(props: NavProps) {
+  //const {open} = props;
+  const state = useSelector((state: RootState) => state.userReducer);
+  const { isLogin } = state.user;
+
+  return (
+    <div className="nav">
+      <Link to="/">
+        <button>Logo</button>
+      </Link>
+      {isLogin ? (
+        <>
+          <button>Login</button>
+          <button>Signup</button>
+        </>
+      ) : (
+        <>
+          <button>Logout</button>
+          <button>Mypage</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Nav;
