9fa53d3dbc2f346fb07d2063a471fb78affaa6dc
Gwan Woo <gwanwoo@Gwans-MacBook-Pro.local>
2021-05-19 11:22:50 +0900

    soundcloud widget 생성

--- mesic-client/src/components/DetailModal/Music.tsx
+++ mesic-client/src/components/DetailModal/Music.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function Music() {
   return (
     <div className="border">
-      Music
+      <iframe
+        height="100%"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https://soundcloud.com/blackstarmot/justin-bieber-peaches-feat-daniel-caesar-giveon&amp;"
+      ></iframe>
     </div>
   );
 }
