15e738c508d9fa60b526b8ddf626f0c9aaa421b4
Gwan Woo <gwanwoo@Gwans-MacBook-Pro.local>
2021-05-26 16:24:25 +0900

    마커 생성 시 모달 창 띄우기 및 버그 수정

--- mesic-client/src/components/DetailModal/PostModal.tsx
+++ mesic-client/src/components/DetailModal/PostModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../reducers";
+import { switchMode } from "../../actions/index";
 import PostMusic from "./PostMusic";
 import PostPhoto from "./PostPhoto";
 import PostMemo from "./PostMemo";
@@ -8,26 +9,57 @@ import axios from "axios";
 //import { read } from "fs";
 
 function PostModal({ postLatLng }: any) {
-  const [postMusic, setPostMusic] = useState<any>(null);
+  const [postMusic, setPostMusic] = useState<{
+    video_Id: string;
+    title: string;
+    thumbnail: string;
+  }>({
+    video_Id: "",
+    title: "",
+    thumbnail: "",
+  });
   const [postImg, setPostImg] = useState<any>("");
   const [postMemo, setPostMemo] = useState<string>("");
 
   const state = useSelector((state: RootState) => state.modeReducer);
-  const { user_id } = state.user;
+  const dispatch = useDispatch();
+  const { user_id, token } = state.user;
+
 
   const postPinData = async () => {
+    console.log("sending : ", {
+      user_id,
+      location: {
+        latitude: postLatLng[1],
+        logitude: postLatLng[0],
+      },
+      music: postMusic,
+      photo: postImg,
+      memo: postMemo,
+    });
     axios
-      .post(`${process.env.REACT_APP_SERVER_URL}/pins/users/%${user_id}`, {
-        user_id,
-        location: {
-          latitude: postLatLng[1],
-          logitude: postLatLng[0],
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/pins`,
+        {
+          user_id,
+          location: {
+            latitude: postLatLng[1],
+            longitude: postLatLng[0],
+          },
+          music: postMusic,
+          photo: postImg,
+          memo: postMemo,
         },
-        music: postMusic,
-        photo: postImg,
-        memo: postMemo,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        dispatch(switchMode("CREATED"));
       })
-      .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
 
--- mesic-client/src/reducers/initialState.tsx
+++ mesic-client/src/reducers/initialState.tsx
@@ -3,7 +3,8 @@ export const initialState = {
   user: {
     isLogin: true,
     mode: "READ",
-    token: "aoejwo223",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwYWJiNWUxNDczZWZhMzMyNTBiM2U1ZiIsIm5hbWUiOiLqtIDsmrAg7KCVIiwiaWF0IjoxNjIyMDAxOTA5LCJleHAiOjE2MjIwMDkxMDl9.N14W1PWbQua3u0HuuDKXIO1dLGfeQT5Gl0SLK6HCXFs",
     email: "test@test.com",
     name: "test",
     profileImg:
@@ -12,10 +13,10 @@ export const initialState = {
     user_id: "60abb5e1473efa33250b3e5f",
   },
   googleToken: "",
-  follow: [ 
+  follow: [
     // 60ab5e1473efa33250b3, 41241afwa5e1473efa3325, 23awefawef5e1473efa33250, 60abb5e1473efawefwa
   ],
   checkedFollow: [
     // 60ab5e1473efa33250b3, 41241afwa5e1473efa3325
-  ]
+  ],
 };
