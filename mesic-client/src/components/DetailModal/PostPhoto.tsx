e1e6c3f4f2df0809b0e8ab749dfd689a25623b05
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-08-14 23:22:55 +0900

    PostPhoto 컴포넌트 분리

--- mesic-client/src/components/DetailModal/PostPhoto.tsx
+++ mesic-client/src/components/DetailModal/PostPhoto.tsx
@@ -1,18 +1,12 @@
 import { useRef, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../reducers";
 import ConfirmModal from "../UI/ConfirmModal";
 import Photo from "./Photo/Photo";
 import NoPhoto from "./Photo/NoPhoto";
 
 function PostPhoto({ postImg, setPostImg }: any) {
-  const imageInput = useRef<any>();
-  const state = useSelector((state: RootState) => state);
-  const { mode } = state.modeReducer.user;
-  const { isLogin } = state.userReducer.user;
-
+  const imageInput = useRef<HTMLInputElement>();
   const [openConfirm, setOpenConfirm] = useState<boolean>(false);
-  const [previewImg, setPreviewImg] = useState<any>({});
+  const [previewImg, setPreviewImg] = useState<any>(null);
   const [fileName, setFileName] = useState<string>("");
 
   useEffect(() => {
@@ -24,11 +18,12 @@ function PostPhoto({ postImg, setPostImg }: any) {
     return;
   }, [postImg]);
 
-  // Photo 업데이트 버튼
-  const handlePostImg = (e: any) => {
-    setPostImg(e.target.files[0]);
-    setPreviewImg(URL.createObjectURL(e.target.files[0]));
-    setFileName("");
+  const handlePostImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files) {
+      setPostImg(e.currentTarget.files[0]);
+      setPreviewImg(URL.createObjectURL(e.currentTarget.files[0]));
+      setFileName("");
+    }
   };
 
   return (
